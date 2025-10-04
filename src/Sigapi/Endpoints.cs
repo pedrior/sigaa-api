96e82ee3fe96ef9fdb55f6b5bee20fe91816f104
Pedro Júnior <pedrojdev@gmail.com>
2025-10-04 08:01:15 -0300

    use public group for mapping center endpoints

--- src/Sigapi/Endpoints.cs
+++ src/Sigapi/Endpoints.cs
@@ -31,7 +31,7 @@ internal static class Endpoints
 
     private static void MapCenterEndpoints(this IEndpointRouteBuilder route)
     {
-        var group = route.MapGroup("centers")
+        var group = route.MapPublicGroup("centers")
             .WithTags(Tags.Centers);
 
         group.MapEndpoint<GetCenterEndpoint>()
